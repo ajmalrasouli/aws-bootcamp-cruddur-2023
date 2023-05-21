3140db0b1544599135bb0fcd977b643f17921cf3
AjmalRasouli <ajrasouli@hotmail.com>
2023-05-21 16:38:47 +0000

    replies

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -1,14 +1,13 @@
 import './ReplyForm.css';
 import React from "react";
 import process from 'process';
-
+import {getAccessToken} from '../lib/CheckAuth';
 
 import ActivityContent  from '../components/ActivityContent';
 
 export default function ReplyForm(props) {
   const [count, setCount] = React.useState(0);
   const [message, setMessage] = React.useState('');
-
   const classes = []
   classes.push('count')
   if (240-count < 0){
@@ -16,27 +15,32 @@ export default function ReplyForm(props) {
   }
 
   const onsubmit = async (event) => {
+    console.log('replyActivity',props.activity)
     event.preventDefault();
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+          activity_uuid: props.activity.uuid,
           message: message
         }),
       });
       let data = await res.json();
       if (res.status === 200) {
         // add activity to the feed
-
         let activities_deep_copy = JSON.parse(JSON.stringify(props.activities))
         let found_activity = activities_deep_copy.find(function (element) {
           return element.uuid ===  props.activity.uuid;
         });
+        console.log('found_activity',found_activity)
         found_activity.replies.push(data)
 
         props.setActivities(activities_deep_copy);
@@ -51,25 +55,19 @@ export default function ReplyForm(props) {
       console.log(err);
     }
   }
-
   const textarea_onchange = (event) => {
     setCount(event.target.value.length);
     setMessage(event.target.value);
   }
-
   let content;
   if (props.activity){
     content = <ActivityContent activity={props.activity} />;
   }
-
   const close = (event)=> {
     if (event.target.classList.contains("reply_popup")) {
       props.setPopped(false)
     }
   }
-
-
-
   if (props.popped === true) {
     return (
       <div className="popup_form_wrap reply_popup" onClick={close}>
