f234943c0bf57bdabd0b50a64ce918b970b01967
AjmalRasouli <ajrasouli@hotmail.com>
2023-05-25 21:47:10 +0000

    add back in the timezones fixes

--- frontend-react-js/src/components/MessageItem.js
+++ frontend-react-js/src/components/MessageItem.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 import { format_datetime, message_time_ago } from '../lib/DateTimeFormats';
 
 export default function MessageItem(props) {
+
+  
   return (
+    
     <div className='message_item'>
       <Link className='message_avatar' to={`/messages/@`+props.message.handle}></Link>
       <div className='message_content'>
@@ -14,10 +17,12 @@ export default function MessageItem(props) {
           </div>{/* activity_identity */}
         </div>{/* message_meta */}
         <div className="message">{props.message.message}</div>
+         
         <div className="created_at" title={format_datetime(props.message.created_at)}>
           <span className='ago'>{message_time_ago(props.message.created_at)}</span> 
         </div>{/* created_at */}
       </div>{/* message_content */}
+    
     </div>
   );
 }
