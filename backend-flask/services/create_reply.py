e2a803564ef0ba3d3908649971630e1b1a6d8c2e
AjmalRasouli <ajrasouli@hotmail.com>
2023-05-25 19:24:31 +0000

    replies

--- backend-flask/services/create_reply.py
+++ backend-flask/services/create_reply.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta, timezone
+
+from lib.db import db
+
 class CreateReply:
   def run(message, cognito_user_id, activity_uuid):
     model = {
@@ -11,15 +14,14 @@ class CreateReply:
 
     if activity_uuid == None or len(activity_uuid) < 1:
       model['errors'] = ['activity_uuid_blank']
-
     if message == None or len(message) < 1:
       model['errors'] = ['message_blank'] 
     elif len(message) > 1024:
       model['errors'] = ['message_exceed_max_chars'] 
-
     if model['errors']:
       # return what we provided
       model['data'] = {
+        
         'message': message,
         'reply_to_activity_uuid': activity_uuid
       }
